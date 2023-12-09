209d523966a58b76a6d711942b22bc2025360da7
Piyumika <piyumikabandula@gmail.com>
2023-12-09 02:38:48 -0500

    Added Category selection (Veg/Nonveg) to Our Menu. Fixed responsiveness over mobile screens.

--- js/menu.js
+++ js/menu.js
@@ -7,36 +7,42 @@ var products = [{"id":0,
   "description":"Classic pizza with tomato sauce, fresh mozzarella cheese, basil, and a drizzle of olive oil.",
   "details":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur officia repellendus quibusdam? Eum dolor quibusdam veritatis provident officiis nisi error molestiae mollitia tempora quae.",
   "imageUrl":"images/pizza-1.jpg",
+  "category":"veg",
   "price":9.99},
   {"id":1,
   "name":"Pepperoni Pizza",
   "description":"Classic pizza with tomato sauce, fresh mozzarella cheese, basil, and a drizzle of olive oil.",
   "details":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur officia repellendus quibusdam? Eum dolor quibusdam veritatis provident officiis nisi error molestiae mollitia tempora quae.",
   "imageUrl":"images/pizza-2.jpg",
+  "category":"veg",
   "price":14.99},
   {"id":2,
   "name":"Vegetarian Pizza",
   "description":"Classic pizza with tomato sauce, fresh mozzarella cheese, basil, and a drizzle of olive oil.",
   "details":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur officia repellendus quibusdam? Eum dolor quibusdam veritatis provident officiis nisi error molestiae mollitia tempora quae.",
   "imageUrl":"images/pizza-3.jpg",
+  "category":"veg",
   "price":9.99},
   {"id":3,
   "name":"BBQ Chicken Pizza",
   "description":"Classic pizza with tomato sauce, fresh mozzarella cheese, basil, and a drizzle of olive oil.",
   "details":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur officia repellendus quibusdam? Eum dolor quibusdam veritatis provident officiis nisi error molestiae mollitia tempora quae.",
   "imageUrl":"images/pizza-4.jpg",
+  "category":"nonveg",
   "price":7.99},
   {"id":4,
   "name":"Hawaiian Pizza",
   "description":"Classic pizza with tomato sauce, fresh mozzarella cheese, basil, and a drizzle of olive oil.",
   "details":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur officia repellendus quibusdam? Eum dolor quibusdam veritatis provident officiis nisi error molestiae mollitia tempora quae.",
   "imageUrl":"images/pizza-5.jpg",
+  "category":"nonveg",
   "price":30.99},
   {"id":5,
   "name":"Meat Lovers Pizza",
   "description":"Classic pizza with tomato sauce, fresh mozzarella cheese, basil, and a drizzle of olive oil.",
   "details":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consequatur officia repellendus quibusdam? Eum dolor quibusdam veritatis provident officiis nisi error molestiae mollitia tempora quae.",
   "imageUrl":"images/pizza-6.jpg",
+  "category":"nonveg",
   "price":23.99}];
 
 function getLocalStorage() {
@@ -78,31 +84,67 @@ function reloadCard() {
   saveToLocalStorage(listCards);
 }
 
+function allPizzas() {
+  getData("all");
+}
+
+function veg() {
+  getData("veg");
+}
+
+function nonveg() {
+  getData("nonveg");
+}
+
+function getData(cat) {
+  list.empty();
+  console.log(cat);
+  products.forEach(function(value, key) {
+    if(value.category == cat) {
+          var newDiv = $(`<div class="item"></div>`).html(`
+          <div class="menu-6-item bg-white">
+            <div class="cont hover-overlay">
+              <img class="menu-6-img img-fluid" onclick="goToDesc(${key});" src="${value.imageUrl}" alt="${value.name}">
+            </div>
+            <div class="item-details">
+              <h5 class="h5-sm" onclick="goToDesc(${key});">${value.name}</h5>
+              <p class="grey-color" onclick="goToDesc(${key});">${value.description}</p>
+              <div class="price" onclick="goToDesc(${key});">$ ${value.price.toLocaleString()}</div>
+              <button onclick="addToCard(${key})">Add To Cart</button>
+            </div>
+          </div>
+        `);
+    
+        if (list.length > 0) {
+          list.append(newDiv);
+        }
+      }else if (cat == "all"){
+        var newDiv = $(`<div class="item"></div>`).html(`
+          <div class="menu-6-item bg-white">
+            <div class="cont hover-overlay">
+              <img class="menu-6-img img-fluid" onclick="goToDesc(${key});" src="${value.imageUrl}" alt="${value.name}">
+            </div>
+            <div class="item-details">
+              <h5 class="h5-sm" onclick="goToDesc(${key});">${value.name}</h5>
+              <p class="grey-color" onclick="goToDesc(${key});">${value.description}</p>
+              <div class="price" onclick="goToDesc(${key});">$ ${value.price.toLocaleString()}</div>
+              <button onclick="addToCard(${key})">Add To Cart</button>
+            </div>
+          </div>
+        `);
+    
+        if (list.length > 0) {
+          list.append(newDiv);
+        }
+      }
+  });
+}
+
+function initApp() {
+  getData("all");
+}
+
 $(document).ready(function() {
   reloadCard();
-
-  var getData = function() {
-    products.forEach(function(value, key) {
-      var newDiv = $(`<div class="item"></div>`).html(`
-        <div class="menu-6-item bg-white">
-          <div class="cont  hover-overlay">
-          <img class="menu-6-img img-fluid" onclick="goToDesc(${key});" src="${value.imageUrl}">
-          </div>
-          <h5 class="h5-sm" onclick="goToDesc(${key});">${value.name}</h5>
-          <p class="grey-color" onclick="goToDesc(${key});">${value.description}</p>
-          <div class="price" onclick="goToDesc(${key});">$ ${value.price.toLocaleString()}</div>
-          <button onclick="addToCard(${key})">Add To Cart</button>
-          </div>
-      `);
-
-      if (list.length > 0) {
-        list.append(newDiv);
-      }
-    });
-  }
-
-  function initApp() {
-    getData();
-  }
   initApp();  
 });
