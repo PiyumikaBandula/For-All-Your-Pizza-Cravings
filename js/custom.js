8335f2c3a48c41171bd476aea0465ba48455f0ca
Piyumika Samarasuriyage <piyumikabandula@gmail.com>
2024-05-14 09:42:10 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
 	"use strict";
 
 	// Function to load head.html file 
-	$('#header1').load('https://piyumikabandula.github.io/Pizza-Website/head.html');
+	$('#header1').load('https://piyumikabandula.github.io/For-All-Your-Pizza-Cravings/head.html');
 
 	// Function to load newsletter.html file
-	$('#newsletter').load('https://piyumikabandula.github.io/Pizza-Website/newsletter.html');
+	$('#newsletter').load('https://piyumikabandula.github.io/For-All-Your-Pizza-Cravings/newsletter.html');
 
 	// Function to load foot.html file
-	$('#footer').load('https://piyumikabandula.github.io/Pizza-Website/foot.html');
+	$('#footer').load('https://piyumikabandula.github.io/For-All-Your-Pizza-Cravings/foot.html');
 });
